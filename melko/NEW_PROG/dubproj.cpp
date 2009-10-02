aa587c35b387d966be377c807003b6d76efa492f
Ann Kallin <1702504+akallin@users.noreply.github.com>
2009-10-02 15:42:58 +0000

    adding new files for double projector stuff'

---
+++ melko/NEW_PROG/dubproj.cpp
@@ -0,0 +1,6 @@
+// Oct 2, 2009 // New double-projector vb qmc program
+
+#include"header.h"
+#include"header_dubproj.h"
+#include<iomanip>
+using namespace std;
---
+++ melko/NEW_PROG/header_dubproj.h
@@ -0,0 +1,11 @@
+// Oct 2, 2009 // Header for new double-projector vb qmc prog (dubproj.cpp)
+
+#include"header.h"
+#include"matrix.h"
+#include<vector>
+using namespace std;
+
+int main()
+{
+
+  
