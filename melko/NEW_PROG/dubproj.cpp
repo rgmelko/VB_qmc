98d5d8ad30ec63cf9e66b9432db11bbba755e61a
Ann Kallin <1702504+akallin@users.noreply.github.com>
2009-10-02 21:38:41 +0000

    working on double projector

--- melko/NEW_PROG/dubproj.cpp
+++ melko/NEW_PROG/dubproj.cpp
@@ -4,3 +4,16 @@
 #include"header_dubproj.h"
 #include<iomanip>
 using namespace std;
+
+class LATTICE
+{
+
+public:
+ 
+  MTRand drand; //drand() gives you a random double precision number
+  MTRand_int32 irand; // irand() gives you a random integer
+
+  int width, height, number_of_bondops, iterations;
+  int number_of_nnbonds, number_of_bonds, number_of_sites;
+  int change_number;  //number of bondops changed per step
+
