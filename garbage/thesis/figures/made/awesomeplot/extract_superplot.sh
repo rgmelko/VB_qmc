a92cee4936293f85111282ec03c63b21a6dc5243
Ann Kallin <1702504+akallin@users.noreply.github.com>
2010-09-28 02:25:47 +0000

    A    awesomeplot/2n1plotdMRG.dat A    awesomeplot/2nplot.dat M    awesomeplot/marea.fig A    awesomeplot/area.eps A    awesomeplot/2nplotdMRG.dat M    awesomeplot/extract_superplot.sh A    awesomeplot/nplot.dat A    awesomeplot/nplotdMRG.dat A    awesomeplot/area2.eps A    awesomeplot/area.agr A    awesomeplot/marea2.eps M    awesomeplot/marea.pdf A    awesomeplot/area2.agr A    awesomeplot/2n1plot.dat M    awesomeplot/marea.eps A    awesomeplot/marea2.fig

--- garbage/thesis/figures/made/awesomeplot/extract_superplot.sh
+++ garbage/thesis/figures/made/awesomeplot/extract_superplot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-awk '{if($1==2) print "2   ",  $2  }'  3leg*
+awk '{if($1==2) print "2   ",  $2  }'  2leg*
 awk '{if($1==3) print "3   ",  $2  }'  3leg*
 awk '{if($1==4) print "4   ",  $2  }'  4leg* 
 awk '{if($1==5) print "5   ", $2  }'   5leg* 
@@ -7,7 +7,7 @@ awk '{if($1==6) print "6   ", $2  }'   6leg*
 awk '{if($1==7) print "7   ", $2  }'   7leg* 
 awk '{if($1==8) print "8   ", $2  }'   8leg* 
 
-awk '{if($1==3) print "2   ",  $2  }'  3leg* 
+awk '{if($1==3) print "2   ",  $2  }'  2leg* 
 awk '{if($1==5) print "3   ",  $2  }'  3leg* 
 awk '{if($1==7) print "4   ",  $2  }'  4leg* 
 awk '{if($1==9) print "5   ", $2 }'   5leg* 
@@ -15,7 +15,7 @@ awk '{if($1==11) print "6   ", $2  }'  6leg*
 awk '{if($1==13) print "7   ", $2  }'  7leg* 
 awk '{if($1==15) print "8   ", $2  }'  8leg* 
 
-awk '{if($1==4) print "2   ",  $2   }'  3leg* 
+awk '{if($1==4) print "2   ",  $2   }'  2leg* 
 awk '{if($1==6) print "3   ",  $2   }'  3leg* 
 awk '{if($1==8) print "4   ",  $2   }'  4leg* 
 awk '{if($1==10) print "5   ", $2  }'  5leg* 
