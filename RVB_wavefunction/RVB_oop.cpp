22f0242e7eaaf047754de012b9ba90adc75a3e72
Roger Melko <rgmelko@uwaterloo.ca>
2011-04-20 21:10:20 +0000

    C(L/2,L/2) implemented

--- RVB_wavefunction/RVB_oop.cpp
+++ RVB_wavefunction/RVB_oop.cpp
@@ -7,42 +7,47 @@
 #include "simparam.h"
 #include "basis.h"
 #include "spinstate.h"
+#include "measure.h"
 
 int main(){
 
     PARAMS param;
     MTRand mrand(param.SEED_); //random number for metropolis
 
-    //param.printBst();
-    //param.printPst();
-
     Basis Vbeta(param);   //bra
     Basis Valpha(param);  //ket
-
-    //Vbeta.print();
-    //Valpha.print();
 
     SpinState Z1(param); //the Sz basis state
 
     int temp;
 
+    Measure Observ; //create measurement object
+	Observ.zero();
+
 	//initialize the spin state
     temp = Z1.SampleRandomState(mrand,Valpha,Vbeta);
  
-	for (int i=0; i<10000; i++){
+    //MC sampling
+	for (int i=0; i<param.MCS_; i++){
+
 		for (int j=0; j<param.numSpin/2; j++){  //sample VB bonds
 			Valpha.TwoBondUpdate(mrand,param,Z1.Sstate);
 			Vbeta.TwoBondUpdate(mrand,param,Z1.Sstate);
 		}
 		temp = Z1.SampleRandomState(mrand,Valpha,Vbeta); //sample spin state
+
+        //measurements
+		Observ.measure_CL2L2(Vbeta, Valpha);
+		Observ.record();
 	}
 
-	Z1.print();
-    Vbeta.print();
-    Valpha.print();
+	Observ.output(param);
 
-	temp = Valpha|Vbeta;
-	cout<<temp<<endl;
+	//Z1.print();
+    //Vbeta.print();
+    //Valpha.print();
+	//temp = Valpha|Vbeta;
+	//cout<<temp<<endl;
 
 
 	return 0;
