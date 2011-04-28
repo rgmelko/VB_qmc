d916e736d264d7ad8d4b40185ff420da808358e6
Roger Melko <rgmelko@uwaterloo.ca>
2011-04-28 16:52:35 +0000

    factor of 2 fixed - swap works?

--- RVB_wavefunction/renyi.h
+++ RVB_wavefunction/renyi.h
@@ -19,7 +19,6 @@ class Renyi
       void zero();
       void measure_H2(const Basis &, const Basis &);
 	  void measure_ratio(const Basis & , const Basis & , const int & );
-      int calc_SWAP_1D(const Basis &, const Basis &, const int &);
       int calc_SWAP_2D(const Basis &, const Basis &, const int &);
       void record();
       void output(PARAMS &);
@@ -75,56 +74,17 @@ void Renyi::measure_ratio(const Basis & A, const Basis & B, const int & x_num){
 
 	Basis Vl(A);   //copy constructors
 	Basis Vr(B);
-	Nloop_den = calc_SWAP_1D(Vl,Vr,x_num);  //1D or 2D: careful
+	Nloop_den = calc_SWAP_2D(Vl,Vr,x_num);  //1D or 2D: careful
 
     int Nloop_num;
 	int r=x_num+1;
 	for (int i=0; i<entropy.size(); i++){
-        Nloop_num = calc_SWAP_1D(A,B,r);
+        Nloop_num = calc_SWAP_2D(A,B,r);
 		entropy.at(i) = 1.0*pow(2,Nloop_num - Nloop_den);
 		r++;
 	}
 
 }//measure_ratio
-
-
-int Renyi::calc_SWAP_1D(const Basis & A, const Basis & B, const int & X){
-
-	inAreg.assign(2*A.LinX,0);
-	for (int i=0; i<X; i++){
-		inAreg.at(i)=1;
-		inAreg.at(i+A.LinX)=1;
-	}
-
-	int Nloop_num; //number of loops in numerator and denominator
-	Basis Vl(A);   //copy constructors
-	Basis Vr(B);
-
-	int a,b, bond1, bond2;
-	for (int i=0; i<X; i++){ //X is the maximum *linear* distance to take region "A"
-		a = i;
-		b = i + A.numSpin/2; //b in the other layer
-		bond1 = Vr.VBasis[a]; 
-		bond2 = Vr.VBasis[b];
-
-		if (inAreg.at(bond2) == 1 )
-			Vr.VBasis[a] = bond2 - A.numSpin/2;
-		else{
-			Vr.VBasis[a] = bond2;
-			Vr.VBasis[bond2] = a;
-		}
-		if (inAreg.at(bond1) == 1 )
-			Vr.VBasis[b] = bond1 + A.numSpin/2;
-		else{
-			Vr.VBasis[b] = bond1;
-			Vr.VBasis[bond1] = b;
-		}
-	}//i
-
-	Nloop_num = Vl|Vr; //new overlap
-
-	return Nloop_num;
-}//calc_SWAP_1D
 
 
 int Renyi::calc_SWAP_2D(const Basis & A, const Basis & B, const int & X){
@@ -200,7 +160,7 @@ void Renyi::output(PARAMS & p){
 	cfout.open("00.renyi",ios::app);
 
     for (int i=0; i<TOTAL_H2.size(); i++)
-	  cfout<<-log(TOTAL_H2.at(i)/(2.0*p.MCS_))<<" ";
+	  cfout<<-log(TOTAL_H2.at(i)/(1.0*p.MCS_))<<" ";
 	cfout<<endl;
 
 	cfout.close();
