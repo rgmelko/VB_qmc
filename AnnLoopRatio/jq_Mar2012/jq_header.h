2c64bcc46976436758cf5d57346cba1ddd6e15b0
Ann Kallin <1702504+akallin@users.noreply.github.com>
2012-03-28 18:51:13 +0000

    minor M    jq_Mar2012/jq_header.h

--- AnnLoopRatio/jq_Mar2012/jq_header.h
+++ AnnLoopRatio/jq_Mar2012/jq_header.h
@@ -409,16 +409,12 @@ i assume vertex legs are labelled like so (Feb 2012):
 
 *******************************************************************************/
 void LOOPS::create__Hlinks()
-{
-  //what does this part do???
-  //might be implying an initial dimerization!!!
-  //yeah totally.  fix it up!
-  //I think it's the same problem for the end too!!
+{ 
+  //The last vertex leg number for a given site
   vector <long long> last (number_of_sites,-99);
 
-  //put initial state as initial vertices in linked list.
-  //THIS IS CONFUSING
-  int vertex=0;
+  //put initial VB state as initial vertices in linked list.
+  int vertex=0; 
   for(int i=0; i<number_of_sites/2; i++){   
     last[nnbonds(superbops(i,0),0)]= 4*vertex+2;
     last[nnbonds(superbops(i,0),1)]= 4*vertex+3;
@@ -438,23 +434,25 @@ void LOOPS::create__Hlinks()
 
   // create Hlinks for the operators after the init VB config.. 
   // but also include the bondops for the other initial VB config 
-  // (on the right side) ... 
+  // (on the right side) |VR> 
 
+  long long templegnum;
   //for the LHS of the bondops
   for(bopnum; bopnum<(number_of_bondops+number_of_sites)/2; bopnum++){
 
     //the top left (first) leg of the bond op
     legnum = 4*bopnum;
 
-
     //join the first leg to the 
-    Hlinks[legnum] = last[nnbonds(superbops(bopnum,0),0)];//does it matter if
-    Hlinks[last[nnbonds(superbops(bopnum,0),0)]] = legnum;//I screw up the order
-    last[nnbonds(superbops(bopnum,0),0)] = legnum + 2; //? because I am
+    templegnum = nnbonds(superbops(bopnum,0),0);
+    Hlinks[legnum] = last[templegnum];//does it matter if
+    Hlinks[last[templegnum]] = legnum;//I screw up the order
+    last[templegnum] = legnum + 2; //? because I am
 
-    Hlinks[legnum+1] = last[nnbonds(superbops(bopnum,0),1)]; 
-    Hlinks[last[nnbonds(superbops(bopnum,0),1)]] = legnum+1;
-    last[nnbonds(superbops(bopnum,0),1)] = legnum + 3;
+    templegnum = nnbonds(superbops(bopnum,0),1);
+    Hlinks[legnum+1] = last[templegnum]; 
+    Hlinks[last[templegnum]] = legnum+1;
+    last[templegnum] = legnum + 3;
   }
 
   /*************************************************************
@@ -484,13 +482,16 @@ void LOOPS::create__Hlinks()
     legnum = 4*bopnum;
 
     //join the first leg to the 
-    Hlinks[legnum] = last[nnbonds(superbops(bopnum,0),0)];//does it matter if
-    Hlinks[last[nnbonds(superbops(bopnum,0),0)]] = legnum;//I screw up the order
-    last[nnbonds(superbops(bopnum,0),0)] = legnum + 2; //? because I am
+    templegnum = nnbonds(superbops(bopnum,0),0);
+    Hlinks[legnum] = last[templegnum];//does it matter if
+    Hlinks[last[templegnum]] = legnum;//I screw up the order
+    last[templegnum] = legnum + 2; //? because I am
 
-    Hlinks[legnum+1] = last[nnbonds(superbops(bopnum,0),1)]; 
-    Hlinks[last[nnbonds(superbops(bopnum,0),1)]] = legnum+1;
-    last[nnbonds(superbops(bopnum,0),1)] = legnum + 3;
+    templegnum = nnbonds(superbops(bopnum,0),1);
+    Hlinks[legnum+1] = last[templegnum]; 
+    Hlinks[last[templegnum]] = legnum+1;
+    last[templegnum] = legnum + 3;   
+
   }
 
 }
